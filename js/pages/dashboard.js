e173221db43e036b2ccce985e19ea171304b2c93
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-05 08:10:49 +0530

    Update project files

--- js/pages/dashboard.js
+++ js/pages/dashboard.js
@@ -3,4 +3,7 @@ import { openTransactionModal } from "../components/transcationForm.js";
 export function initDashboard (){
     document.querySelector("#quickAddBtn")
             .addEventListener("click", openTransactionModal)
+    
+    document.querySelector("#cancelBtn")\
+            .add
 }
