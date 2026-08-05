931a738da7053ec8313213667ed56b72a236e476
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-05 08:34:33 +0530

    Update project files

--- js/pages/dashboard.js
+++ js/pages/dashboard.js
@@ -11,11 +11,16 @@ export function initDashboard (){
     let form = document.querySelector("form")
     form.addEventListener("submit", (e)=>{
         e.preventDefault();
-        console.log(e);
+        console.log(e.target[0].value);
+        console.log(e.target[1].value);
+        console.log(e.target[2].value);
+        console.log(e.target[3].value);
+        console.log(e.target[4].value);
+        // console.log(e.target[5].value);
         
-        console.log(e.srcElement);
+        // console.log(e.srcElement);
         
-        console.log(ine.value);
+        // console.log(ine.value);
                 
     })
 
