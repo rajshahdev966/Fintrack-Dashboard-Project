f759004bb336e052f5b47718447e3c1f7bc2f9af
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-05 06:59:28 +0530

    Auto sync changes

--- js/pages/dashboard.js
+++ js/pages/dashboard.js
@@ -1,6 +1,5 @@
 import { openTransactionModal } from "../components/transcationForm";
 
 export function initDashboard (){
-    const quickAddBtn = document.querySelector("#quickAddBtn");
-    quickAddBtn.addEventListener('click', openTransactionModal)
+    
 }
