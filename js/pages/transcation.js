5fac32174659f372ab0088f184572c2ea01d6d4f
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-05 09:11:29 +0530

    Updated portfolio content

--- js/pages/transcation.js
+++ js/pages/transcation.js
@@ -3,12 +3,12 @@ import { closeTransactionModal, openTransactionModal, transModalSubmit } from ".
 export function initTransPage(){
     
     console.log("TransPage Work!");
-    
+
     document.querySelector("#add-new-btn")
     .addEventListener("click", openTransactionModal)
     
     document.querySelector("#cancelBtn")
-    .addEventListener(closeTransactionModal)
+    .addEventListener("click", closeTransactionModal)
     
     transModalSubmit();
 }
