54899f72a5c21847c8898bc0b5bc23fb08824631
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-05 10:06:48 +0530

    Modified website assets

--- js/pages/transcation.js
+++ js/pages/transcation.js
@@ -1,7 +1,39 @@
 import { closeTransactionModal, openTransactionModal, transModalSubmit } from "../components/transcationForm.js";
+import { allTrans } from "../core/storage.js";
 
 export let allTransUpdate = ()=>{
-    let 
+    let transTable = document.querySelector("tbody");
+    transTable.innerHTML = ``
+    allTrans.forEach((transcation, index)=>[
+        transTable.innerHTML += `<tr class="hover:bg-(--surface-bright) transition">
+
+                        <td class="px-6 py-6 font-medium">
+                            ${all}
+                        </td>
+
+                        <td class="px-6 py-6">
+
+                            <span
+                                class="rounded-full px-3 py-1 text-xs font-semibold">
+                                Shopping
+                            </span>
+
+                        </td>
+
+                        <td class="px-6 py-6 text-(--text-secondary)">
+                            Amazon Premium Web Services Monthly Sub
+                        </td>
+
+                        <td class="px-6 py-6 text-right font-bold text-(--danger)">
+                            -$142.00
+                        </td>
+
+                        <td class="px-6 py-6 text-center">
+                            ⋮
+                        </td>
+
+                    </tr>`
+    ])
 }
 
 export function initTransPage(){
