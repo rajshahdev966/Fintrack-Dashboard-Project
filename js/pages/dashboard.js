2a4f1163791c07105f1cae3df37cd0c99c4575ca
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-05 09:30:09 +0530

    Auto sync changes

--- js/pages/dashboard.js
+++ js/pages/dashboard.js
@@ -1,4 +1,18 @@
 import { closeTransactionModal, openTransactionModal, transModalSubmit } from "../components/transcationForm.js";
+import { allTrans } from "../core/storage.js";
+
+let dashboardTransUpd = ()=>{
+    let recentList = document.querySelector("#recent-trans-list")
+    allTrans.forEach((transcation, index)=>{
+        recentList += `<div class="flex justify-between items-center border-b pb-4 border-(--text-primary)">
+                    <div>
+                        <h3 class="font-semibold text-(--text-primary)">${}</h3>
+                        <p class="text-(--text-muted) text-sm">Tech & Gear</p>
+                    </div>
+                    <span class="text-(--danger) font-bold">-$1,299</span>
+                </div>` 
+    })
+}
 
 export function initDashboard (){
 
