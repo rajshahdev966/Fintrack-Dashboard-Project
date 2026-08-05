6fb8915f0ccee5b35e530fb93d6383008cb4efec
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-05 23:42:45 +0530

    Modified website assets

--- js/pages/settings.js
+++
@@ -1,9 +0,0 @@
-export const delAllData = () => {
-  const delAllBtn = document.querySelector("#del-all-data-btn");
-  delAllBtn.addEventListener("click", () => {
-    localStorage.clear();
-  });
-};
-export const initSettings = () => {
-  delAllData();
-};
