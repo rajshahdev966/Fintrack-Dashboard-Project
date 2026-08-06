f11cda219576662e9f2f2d1dc901916a77443066
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-06 14:29:50 +0530

    Updated portfolio content

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ import { dashboardTransUpd, initDashboard } from "./pages/dashboard.js";
 import { initSettings } from "./pages/settings.js";
 import { allTransUpdate, initTransPage } from "./pages/transcation.js";
 
+console.log("Main Work");
+
+
 const page = document.body.id;
 
 switch (page) {
