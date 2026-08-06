88734c566cdf17190abc81d649aca40c2315c65e
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-06 14:52:05 +0530

    Updated portfolio content

--- js/pages/settings.js
+++ js/pages/settings.js
@@ -2,17 +2,12 @@ import { allTrans } from "../core/storage.js";
 import { dashboardTransUpd } from "./dashboard.js";
 import { allTransUpdate } from "./transcation.js";
 
-export const clearLocalStor = () => {
-    let allClearBtn = document.querySelector("#all-clear-btn");
-    allClearBtn.addEventListener("click", () => {
-      console.log("Button Working");
-      localStorage.removeItem("savedTrans")
-    allTrans.splice(0)
-    dashboardTransUpd();
-    allTransUpdate();
-  });
-};
+
+
 export const initSettings = ()=>{
     console.log("Settigns working");
-    clearLocalStor();
+    let allClearBtn = document.querySelector("#all-clear-btn");
+    allClearBtn.addEventListener("click", () => {
+      console.log("Button Working")
+    });
 }
