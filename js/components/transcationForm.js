cd32c93c83414bf0a1d0bfdf768c1e75c94b9e4b
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-06 14:12:57 +0530

    Auto sync changes

--- js/components/transcationForm.js
+++ js/components/transcationForm.js
@@ -3,38 +3,44 @@ import { allAmountUpdate, dashboardTransUpd } from "../pages/dashboard.js";
 import { allTransUpdate, totalTransUpdate } from "../pages/transcation.js";
 
 export function openTransactionModal() {
-    const formCard = document.querySelector("#transFormCard");
-    formCard.classList.remove("hidden");
+  const formCard = document.querySelector("#transFormCard");
+  formCard.classList.remove("hidden");
 }
 
 export function closeTransactionModal() {
-    const formCard = document.querySelector("#transFormCard");
-    formCard.classList.add("hidden");
+  const formCard = document.querySelector("#transFormCard");
+  formCard.classList.add("hidden");
 }
 
-export function transModalSubmit(){
-    let form = document.querySelector("form");
-    form.addEventListener("submit", (e)=>{
-        e.preventDefault();
-        let transcation = {
-            type: e.target[0].value,
-            amount: e.target[0].value == "Income" ? `+${e.target[1].value}`: `-${e.target[1].value}`,
-            purpose: e.target[2].value,
-            category: e.target[3].value,
-            date: e.target[4].value,
-        }
-        allTrans.push(transcation);
-        saveTrans();
-        if(document.body.id == "dashboard-page") allAmountUpdate();
-        if(document.body.id == "dashboard-page") dashboardTransUpd();
-        if(document.body.id == "transactions-page") totalTransUpdate();
-        if(document.body.id == "transactions-page") allTransUpdate();
-        form.reset();
-        closeTransactionModal();
-        console.log("Transcation Modal Working");
-        
-        // dashboardTransUpd();
-        // allTransUpdate();
-        
-    })
+export function transModalSubmit() {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let transcation = {
+      type: e.target[0].value,
+      amount:
+        e.target[0].value == "Income"
+          ? `+${e.target[1].value}`
+          : `-${e.target[1].value}`,
+      purpose: e.target[2].value,
+      category: e.target[3].value,
+      date: e.target[4].value,
+    };
+    allTrans.push(transcation);
+    saveTrans();
+    if (document.body.id == "dashboard-page") {
+      allAmountUpdate();
+      dashboardTransUpd();
+    }
+    if (document.body.id == "transactions-page") {
+      totalTransUpdate();
+      allTransUpdate();
+    }
+    form.reset();
+    closeTransactionModal();
+    console.log("Transcation Modal Working");
+
+    // dashboardTransUpd();
+    // allTransUpdate();
+  });
 }
