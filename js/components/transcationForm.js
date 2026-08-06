d118b609c0c2d03ac6a440e9b194bd62ad776e73
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-06 14:54:20 +0530

    Modified website assets

--- js/components/transcationForm.js
+++ js/components/transcationForm.js
@@ -18,10 +18,7 @@ export function transModalSubmit() {
     e.preventDefault();
     let transcation = {
       type: e.target[0].value,
-      amount:
-        e.target[0].value == "Income"
-          ? `+${e.target[1].value}`
-          : `-${e.target[1].value}`,
+      amount: e.target[1].value,
       purpose: e.target[2].value,
       category: e.target[3].value,
       date: e.target[4].value,
