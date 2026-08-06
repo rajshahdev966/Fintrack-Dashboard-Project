892d2c643186905dec61c6dfb8a9f0726e1c0551
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-06 14:20:52 +0530

    Modified website assets

--- js/pages/settings.js
+++ js/pages/settings.js
@@ -1,9 +1,9 @@
-export const clearLocalSotr = () => {
+export const clearLocalStor = () => {
   let allClearBtn = document.querySelector("#all-clear-btn");
   allClearBtn.addEventListener("click", () => {
     localStorage.clear();
   });
 };
 export const initSettings = ()=>{
-    clearLocalSotr();
+    clearLocalStor();
 }
