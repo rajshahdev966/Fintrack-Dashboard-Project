19a1e8fc7e345a1e99ff45804feac4dd0e49fad7
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-08 18:17:09 +0530

    Update project files

--- js/components/charts.js
+++ js/components/charts.js
@@ -1,6 +1,7 @@
 export const updateGraph = ()=>{
-    let totalGraph = document.querySelector("#")
+    let totalGraph = document.querySelector("#total-graph")
     let incomeGraph = document.querySelector("#income-graph")
     let expenseGrpah = document.querySelector("#expense-graph")
-    incomeGraph.style.height = `${}`
+    incomeGraph.style.height = `${(income/total) * (totalGraph.clientHeight)}`
+    incomeGraph.style.height = `${(expense/total) * (totalGraph.clientHeight)}`
 }
