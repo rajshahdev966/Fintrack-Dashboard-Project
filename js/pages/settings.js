cb0ad72df10aee025f9f6f861a095db59d948f73
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-09 00:48:35 +0530

    Update project files

--- js/pages/settings.js
+++ js/pages/settings.js
@@ -3,23 +3,23 @@ import { dashboardTransUpd } from "./dashboard.js";
 import { allTransUpdate } from "./transcation.js";
 
 export const themeSwitch = ()=>{
-  const switchBtn = document.querySelector("#switch-btn");
-  const switchLogo = document.querySelector("#switch-logo");
   const themeLogic = ()=>{
     if(darkOn){
-      console.log("Theme should be dark");
       
       document.body.classList.add("dark")
-      switchLogo.classList.replace("ri-moon-fill", "ri-sun-fill");
+      
+      // switchLogo.classList.replace("ri-moon-fill", "ri-sun-fill");
       
       
     }else{
-      console.log("Theme should be light");
       document.body.classList.remove("dark")
-      switchLogo.classList.replace("ri-sun-fill", "ri-moon-fill");
+      // switchLogo.classList.replace("ri-sun-fill", "ri-moon-fill");
       
     }
   }
+  if(!document.body.id == "settings-page") return;
+  const switchBtn = document.querySelector("#switch-btn");
+  const switchLogo = document.querySelector("#switch-logo");
   switchBtn.addEventListener("click", ()=>{
     console.log("Theme Chnaged");
     toggleTheme();
