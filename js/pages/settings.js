48a84897756af9e9f9ac7272fc3b7cdaa3be4631
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-09 00:12:33 +0530

    Modified website assets

--- js/pages/settings.js
+++ js/pages/settings.js
@@ -2,6 +2,12 @@ import { allTrans } from "../core/storage.js";
 import { dashboardTransUpd } from "./dashboard.js";
 import { allTransUpdate } from "./transcation.js";
 
+export const themeSwitch = ()=>{
+  const switchBtn = document.querySelector("theme-switch-btn");
+  switchBtn.addEventListener("click", ()=>{
+    
+  })
+}
 
 
 export const initSettings = ()=>{
