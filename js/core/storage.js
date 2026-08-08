2104ecefb4c02e07b240e1956958a4475d54e739
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-09 00:24:18 +0530

    Auto sync changes

--- js/core/storage.js
+++ js/core/storage.js
@@ -1,4 +1,5 @@
 export let allTrans = JSON.parse(localStorage.getItem('savedTrans')) ?? [];
+export let darkOn = localStorage.getItem('darkOn');
 
 export function saveTrans (){
     localStorage.setItem('savedTrans', JSON.stringify(allTrans));
