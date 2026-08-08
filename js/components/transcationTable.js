f493f7eaca3d3830d7f50063b77ad6b4ed52c418
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-09 00:04:04 +0530

    Modified website assets

--- js/components/transcationTable.js
+++ js/components/transcationTable.js
@@ -1,9 +1,14 @@
 import { allTrans } from "../core/storage.js";
+import { allTransUpdate } from "../pages/transcation.js";
 
 export const transSort = () => {
   const incSortBtn = document.querySelector("#inc-sort-btn");
   const expSortBtn = document.querySelector("#exp-sort-btn");
+  const allBtn = document.querySelector("#all-btn")
   let remove;
+  allBtn.addEventListener("click", ()=>{
+    allTransUpdate();
+  })
   incSortBtn.addEventListener("click", () => {
     remove = "Expense";
     console.log("Hi");
