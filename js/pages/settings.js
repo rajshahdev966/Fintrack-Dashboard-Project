02b781541b970ecff7f849019b428dc38256bb2f
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-09 00:54:59 +0530

    Auto sync changes

--- js/pages/settings.js
+++ js/pages/settings.js
@@ -2,25 +2,24 @@ import { allTrans, darkOn, saveTheme, toggleTheme } from "../core/storage.js";
 import { dashboardTransUpd } from "./dashboard.js";
 import { allTransUpdate } from "./transcation.js";
 
-export let themeLogic;
+export const themeLogic = ()=>{
+  if(darkOn){
+    console.log("Theme should be dark");
+    document.body.classList.add("dark")
+    // switchLogo.classList.replace("ri-moon-fill", "ri-sun-fill");
+    
+    
+  }else{
+    console.log("Theme should be light");
+    document.body.classList.remove("dark")
+    // switchLogo.classList.replace("ri-sun-fill", "ri-moon-fill");
+    
+  }
+}
 
 export const themeSwitch = ()=>{
   const switchBtn = document.querySelector("#switch-btn");
   const switchLogo = document.querySelector("#switch-logo");
-  themeLogic = ()=>{
-    if(darkOn){
-      console.log("Theme should be dark");
-      document.body.classList.add("dark")
-      switchLogo.classList.replace("ri-moon-fill", "ri-sun-fill");
-      
-      
-    }else{
-      console.log("Theme should be light");
-      document.body.classList.remove("dark")
-      switchLogo.classList.replace("ri-sun-fill", "ri-moon-fill");
-      
-    }
-  }
   switchBtn.addEventListener("click", ()=>{
     console.log("Theme Chnaged");
     toggleTheme();
