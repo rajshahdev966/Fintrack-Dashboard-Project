7342bef869108df3cf2f3f8b809247ace4670ab8
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-09 00:59:08 +0530

    Auto sync changes

--- js/pages/settings.js
+++ js/pages/settings.js
@@ -3,6 +3,8 @@ import { dashboardTransUpd } from "./dashboard.js";
 import { allTransUpdate } from "./transcation.js";
 
 export const themeLogic = ()=>{
+  console.log("darkon =" + darkOn);
+  
   if(darkOn){
     console.log("Theme should be dark");
     document.body.classList.add("dark")
@@ -11,7 +13,7 @@ export const themeLogic = ()=>{
     
   }else{
     console.log("Theme should be light");
-    document.body.classList.remove("dark")
+    document.body.classList.remove("dark");
     // switchLogo.classList.replace("ri-sun-fill", "ri-moon-fill");
     
   }
