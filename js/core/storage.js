c8a03bb56e7803a0315226323c8b308b89631745
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-09 01:11:23 +0530

    Updated portfolio content

--- js/core/storage.js
+++ js/core/storage.js
@@ -1,10 +1,14 @@
 export let allTrans = JSON.parse(localStorage.getItem('savedTrans')) ?? [];
 export let darkOn = JSON.parse(localStorage.getItem('darkOn')) ?? false;
 
+console.log("Storage: ", darkOn);
+
+
 export function saveTrans (){
     localStorage.setItem('savedTrans', JSON.stringify(allTrans));
 }
 export const saveTheme = ()=>{
+    console.log("Storage: ", darkOn);
     localStorage.setItem('dakrOn', JSON.stringify(darkOn))
 }
 export const toggleTheme = ()=>{
