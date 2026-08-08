c278513a3d10878ebc49427bd03ef4383e755a89
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-08 23:51:44 +0530

    Updated portfolio content

--- js/components/transcationTable.js
+++ js/components/transcationTable.js
@@ -1,9 +1,18 @@
+import { allTrans } from "../core/storage";
+
 export const transSort = ()=>{
     const incSortBtn = document.querySelector("#inc-sort-btn");
     const expSortBtn = document.querySelector("#exp-sort-btn");
+    let sortBy;
     incSortBtn.addEventListener("click", ()=>{
-        
+        sortBy = "Income";
     })
+    expSortBtn.addEventListener("click", ()=>{
+        sortBy = "Expense";
+    })
+    allTrans.forEach(element => {
+        
+    });
 
 
 
