82d580005fbeae7618aaf0a52a3b3a4bf877f413
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-08 23:19:45 +0530

    Update project files

--- js/components/transcationForm.js
+++ js/components/transcationForm.js
@@ -65,8 +65,8 @@ export const openEditModal = (index) => {
     allTrans[index].date = e.target[4].value;
     saveTrans();
     console.log(allTrans[index]);
-    // totalTransUpdate();
-    // allTransUpdate();
+    totalTransUpdate();
+    allTransUpdate();
     formCard.reset();
     closeTransactionModal();
   });
