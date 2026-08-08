4782001e2973a6ce0f232a95b986c56453dbf1c2
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-08 18:13:44 +0530

    Updated portfolio content

--- js/components/charts.js
+++ js/components/charts.js
@@ -1,3 +1,6 @@
-export const  = ()=>{
-
+export const updateGraph = ()=>{
+    let totalGraph = document.querySelector("#")
+    let incomeGraph = document.querySelector("#income-graph")
+    let expenseGrpah = document.querySelector("#expense-graph")
+    incomeGraph.style.height = `${}`
 }
