fdb9833fdd724cddf8f327a0e857dca6a53f7c34
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-08 21:48:52 +0530

    Auto sync changes

--- js/components/charts.js
+++ js/components/charts.js
@@ -3,9 +3,9 @@ import { expense, income, netWorth, total } from "../pages/dashboard.js"
 export const updateGraph = ()=>{
     let totalGraph = document.querySelector("#total-graph")
     let incomeGraph = document.querySelector("#income-graph")
-    let expenseGrpah = document.querySelector("#expense-graph")
+    let expenseGraph = document.querySelector("#expense-graph")
     incomeGraph.style.height = `${((income/total) * (totalGraph.clientHeight))}px`
-    incomeGraph.style.height = `${((expense/total) * (totalGraph.clientHeight))}px`
+    expenseGraph.style.height = `${((expense/total) * (totalGraph.clientHeight))}px`
     console.log(income);
     console.log(expense);
     console.log(total);
