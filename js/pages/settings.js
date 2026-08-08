7a7ccbab4248e77a9bb2be6f01914a02d76ca28a
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-09 00:23:27 +0530

    Updated portfolio content

--- js/pages/settings.js
+++ js/pages/settings.js
@@ -4,9 +4,14 @@ import { allTransUpdate } from "./transcation.js";
 
 export const themeSwitch = ()=>{
   const switchBtn = document.querySelector("switch-btn");
-  const switchLogo = document.querySelector("switch-btn");
+  const switchLogo = document.querySelector("switch-logo");
   switchBtn.addEventListener("click", ()=>{
-    
+    if(darkOn){
+      document.body.classList.add(dark)
+      switchLogo.classList.replace(ri-moon-fill, ri-sun-fill);
+    }else{
+      switchLogo.classList.replace(ri-sun-fill, ri-moon-fill);
+    }
   })
 }
 
