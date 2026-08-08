e8b8744a1be6cab3d5e50f1b41f239897c49c237
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-08 22:05:21 +0530

    Update project files

--- js/pages/transcation.js
+++ js/pages/transcation.js
@@ -1,10 +1,7 @@
 import { closeTransactionModal, openTransactionModal, transModalSubmit } from "../components/transcationForm.js";
 import { allTrans, saveTrans } from "../core/storage.js";
-export const delTrans = (index)=>{
-    allTrans.splice(index , 1);
-    saveTrans();
-    allTransUpdate();
-}
+
+export let delTrans;
 
 export let allTransUpdate = ()=>{
     console.log("all Trans Update working");
@@ -13,34 +10,34 @@ export let allTransUpdate = ()=>{
     transTable.innerHTML = ``
     allTrans.forEach((transcation, index)=>{
         transTable.innerHTML += `<tr class="hover:bg-(--surface-bright) transition">
-
-                        <td class="px-6 py-6 font-medium">
-                            ${transcation.date}
-                        </td>
-
-                        <td class="px-6 py-6">
-
-                            <span
-                                class="rounded-full px-3 py-1 text-xs font-semibold">
-                                ${transcation.category}
-                            </span>
-
-                        </td>
-
-                        <td class="px-6 py-6 text-(--text-secondary)">
-                            ${transcation.purpose}
-                        </td>
-
-                        <td class="px-6 py-6 text-right font-bold text-(--${transcation.type == "Income" ? "success" : "danger"})">
-                            ${transcation.type == "Income" ? "+" : "-"}${transcation.amount}
-                        </td>
-
-                        <td class="px-6 py-6 text-center flex items-center justify-center gap-4 text-3xl">
-                            <span class="hover:text-(--danger) cursor-pointer" onclick="delTrans(${index})"><i class="ri-close-circle-fill"></i></span>
-                            <span class="hover:text-(--primary) cursor-pointer" ><i class="ri-pencil-fill"></i></span>
-                        </td>
-                    </tr>`
-})
+        
+        <td class="px-6 py-6 font-medium">
+        ${transcation.date}
+        </td>
+        
+        <td class="px-6 py-6">
+        
+        <span
+        class="rounded-full px-3 py-1 text-xs font-semibold">
+        ${transcation.category}
+        </span>
+        
+        </td>
+        
+        <td class="px-6 py-6 text-(--text-secondary)">
+        ${transcation.purpose}
+        </td>
+        
+        <td class="px-6 py-6 text-right font-bold text-(--${transcation.type == "Income" ? "success" : "danger"})">
+        ${transcation.type == "Income" ? "+" : "-"}${transcation.amount}
+        </td>
+        
+        <td class="px-6 py-6 text-center flex items-center justify-center gap-4 text-3xl">
+        <span class="hover:text-(--danger) cursor-pointer" onclick="delTrans(${index})"><i class="ri-close-circle-fill"></i></span>
+        <span class="hover:text-(--primary) cursor-pointer" ><i class="ri-pencil-fill"></i></span>
+        </td>
+        </tr>`
+    })
 }
 
 export const totalTransUpdate = ()=>{
@@ -52,7 +49,7 @@ export const totalTransUpdate = ()=>{
 export function initTransPage(){
     
     console.log("TransPage Work!");
-
+    
     document.querySelector("#add-new-btn")
     .addEventListener("click", openTransactionModal)
     
@@ -61,5 +58,11 @@ export function initTransPage(){
     
     transModalSubmit();
     totalTransUpdate();
+    allTransUpdate();
+}
+
+delTrans = (index)=>{
+    allTrans.splice(index , 1);
+    saveTrans();
     allTransUpdate();
 }
