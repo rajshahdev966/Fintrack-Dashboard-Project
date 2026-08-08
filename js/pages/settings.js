f066ec359909e90cadab1af6b698eb8c8b38d52b
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-09 00:27:49 +0530

    Update project files

--- js/pages/settings.js
+++ js/pages/settings.js
@@ -1,24 +1,30 @@
-import { allTrans } from "../core/storage.js";
+import { allTrans, darkOn } from "../core/storage.js";
 import { dashboardTransUpd } from "./dashboard.js";
 import { allTransUpdate } from "./transcation.js";
 
 export const themeSwitch = ()=>{
   const switchBtn = document.querySelector("switch-btn");
   const switchLogo = document.querySelector("switch-logo");
-  switchBtn.addEventListener("click", ()=>{
+  const themeLogic = ()=>{
     if(darkOn){
       document.body.classList.add(dark)
       switchLogo.classList.replace(ri-moon-fill, ri-sun-fill);
+      
     }else{
       switchLogo.classList.replace(ri-sun-fill, ri-moon-fill);
     }
+  }
+  switchBtn.addEventListener("click", ()=>{
+    console.log("Theme Chnaged");
+    
+    darkOn != darkOn;
+    themeLogic();
   })
 }
 
 
 export const initSettings = ()=>{
-
-    
+themeSwitch();    
 
 }
 
