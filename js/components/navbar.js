ceed9335a090c797470699a4bb9a98b7397b28e0
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-09 22:47:00 +0530

    Updated portfolio content

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,7 +1,11 @@
 export const openNavOnMobile = ()=>{
     const hamBtn = document.querySelector("#ham-btn");
     const navBar = document.querySelector("nav")
-    hamBtn.addEventListener(()=>{
+    hamBtn.addEventListener("click", ()=>{
+        console.log("Hello before");
         navBar.classList.toggle("translate-x-0")
+        navBar.classList.remove("-tra")
+        console.log("Hello after");
+        
     })
 }
