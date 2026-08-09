d63d8ac776ddf54bce320b87b117a60056784fb4
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-09 09:12:57 +0530

    Updated portfolio content

--- js/pages/settings.js
+++ js/pages/settings.js
@@ -2,41 +2,36 @@ import { allTrans, darkOn, saveTheme, toggleTheme } from "../core/storage.js";
 import { dashboardTransUpd } from "./dashboard.js";
 import { allTransUpdate } from "./transcation.js";
 
-export const themeLogic = ()=>{
+export const themeLogic = () => {
   console.log("darkon = " + darkOn);
-  
-  if(darkOn){
-    console.log("Theme should be dark");
-    document.body.classList.add("dark")
-    if(document.body.id == "settings-page")
-    // switchLogo.classList.replace("ri-moon-fill", "ri-sun-fill");
-    
-    
-  }else{
-    console.log("Theme should be light");
+
+  if (darkOn) {
+    document.body.classList.add("dark");
+    if (document.body.id == "settings-page") {
+      switchLogo.classList.replace("ri-moon-fill", "ri-sun-fill");
+    }
+  } else {
     document.body.classList.remove("dark");
-    // switchLogo.classList.replace("ri-sun-fill", "ri-moon-fill");
-    
+    if (document.body.id == "settings-page") {
+      switchLogo.classList.replace("ri-sun-fill", "ri-moon-fill");
+    }
   }
   console.log(document.body);
-  
-  document.body.classList.contains("dark")
-}
 
-export const themeSwitch = ()=>{
+  document.body.classList.contains("dark");
+};
+
+export const themeSwitch = () => {
   const switchBtn = document.querySelector("#switch-btn");
   const switchLogo = document.querySelector("#switch-logo");
-  switchBtn.addEventListener("click", ()=>{
+  switchBtn.addEventListener("click", () => {
     console.log("Theme Chnaged");
     toggleTheme();
     themeLogic();
     saveTheme();
-  })
-}
+  });
+};
 
-
-export let initSettings = ()=>{
-themeSwitch();    
-
-}
-
+export let initSettings = () => {
+  themeSwitch();
+};
