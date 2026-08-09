78c3fe3f82ed28a7825fc6df321471ba153846b8
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-09 22:42:50 +0530

    Updated portfolio content

---
+++ js/components/navbar.js
@@ -0,0 +1,7 @@
+export const openNavOnMobile = ()=>{
+    const hamBtn = document.querySelector("#ham-btn");
+    const navBar = document.querySelector("nav")
+    hamBtn.addEventListener(()=>{
+        navBar.classList.toggle
+    })
+}
