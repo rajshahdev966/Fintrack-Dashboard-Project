f129771990443689e13f54380378bf11c9ba7eab
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-09 20:36:53 +0530

    Modified website assets

--- js/pages/transcation.js
+++ js/pages/transcation.js
@@ -11,13 +11,13 @@ export let allTransUpdate = ()=>{
     if(!transTable) return;
     transTable.innerHTML = ``
     allTrans.forEach((transcation, index)=>{
-        transTable.innerHTML += `<tr class="hover:bg-(--surface-bright) transition">
+        transTable.innerHTML += `<tr class="hover:bg-(--surface-bright) transition block md:table-row">
         
-        <td class="px-6 py-6 font-medium">
+        <td class="px-6 py-6 font-medium block md:table-cell">
         ${transcation.date}
         </td>
         
-        <td class="px-6 py-6">
+        <td class="px-6 py-6 block md:table-cell">
         
         <span
         class="rounded-full px-3 py-1 text-xs font-semibold">
@@ -26,17 +26,17 @@ export let allTransUpdate = ()=>{
         
         </td>
         
-        <td class="px-6 py-6 text-(--text-secondary)">
+        <td class="px-6 py-6 text-(--text-secondary) block md:table-cell">
         ${transcation.purpose}
         </td>
         
-        <td class="px-6 py-6 text-right font-bold text-(--${transcation.type == "Income" ? "success" : "danger"}) w-min">
+        <td class="px-6 py-6 text-right font-bold text-(--${transcation.type == "Income" ? "success" : "danger"}) w-min block md:table-cell">
         <span>${transcation.type == "Income" ? "+" : "-"}</span>
         <span id="currency-display"></span>
         ${transcation.amount}
         </td>
         
-        <td class="px-6 py-6 text-center flex items-center justify-center gap-4 text-3xl">
+        <td class="px-6 py-6 text-center flex items-center justify-center gap-4 text-3xl md:">
         <span class="hover:text-(--danger) cursor-pointer" onclick="delTrans(${index})"><i class="ri-close-circle-fill"></i></span>
         <span class="hover:text-(--primary) cursor-pointer" onclick="editTrans(${index})"><i class="ri-pencil-fill"></i></span>
         </td>
