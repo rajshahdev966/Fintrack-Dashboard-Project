79fe71c464d3a7b7861ef189a450b70abea044ee
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-08-10 08:32:16 +0530

    Auto sync changes

--- js/components/transcationForm.js
+++ js/components/transcationForm.js
@@ -1,11 +1,4 @@
-import { allTrans, saveTrans } from "../core/storage.js";
-import {
-  allAmountUpdate,
-  dashboardTransUpd,
-  netWorth,
-  income,
-  expense,
-} from "../pages/dashboard.js";
+import { allTrans, saveTrans, saveTranscation } from "../core/storage.js";
 import { allTransUpdate, totalTransUpdate } from "../pages/transcation.js";
 import { updateGraph } from "./charts.js";
 
@@ -33,29 +26,18 @@ const getTranscationFromForm = (form)=>{
     };
 }
 
-export function transModalSubmit() {
-  let form = document.querySelector("form");
+export function transModalSubmit(onSuccess) {
+  const form = document.querySelector("form");
+  
   form.addEventListener("submit", (e) => {
     if(formEdit) return;
     e.preventDefault();
-    let transcation = {
-      type: e.target[0].value,
-      amount: Number(e.target[1].value),
-      purpose: e.target[2].value,
-      category: e.target[3].value,
-      date: e.target[4].value,
-    };
-    allTrans.push(transcation);
-    saveTrans();
-    if (document.body.id == "dashboard-page") {
-      allAmountUpdate();
-      dashboardTransUpd();
-      updateGraph();
-    }
-    if (document.body.id == "transactions-page") {
-      totalTransUpdate();
-      allTransUpdate();
-    }
+
+    let transcation = getTranscationFromForm(form)
+    saveTranscation(transcation);
+    
+   onSuccess();
+
     form.reset();
     closeModal();
   });
